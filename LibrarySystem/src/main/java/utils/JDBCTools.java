f5722b1b38c93756937b1c2fa08ae029dde5abbb
F1sh-Yu <jasonyee616@163.com>
2020-05-21 15:51:42 +0800

    add c3p0

--- LibrarySystem/src/main/java/utils/JDBCTools.java
+++ LibrarySystem/src/main/java/utils/JDBCTools.java
@@ -1,21 +1,25 @@
 package utils;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCTools {
-    private static String DBDriver = "com.mysql.cj.jdbc.Driver";
-    private static String url = "jdbc:mysql://localhost:3306/Library?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
-    private static String username = "root";
-    private static String password = "wqnmlgb!";
+//    private static String DBDriver = "com.mysql.cj.jdbc.Driver";
+//    private static String url = "jdbc:mysql://localhost:3306/Library?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
+//    private static String username = "root";
+//    private static String password = "wqnmlgb!";
+    private static DataSource dataSource = new ComboPooledDataSource("testc3p0");
 
     public static Connection getConnection(){
         Connection connection = null;
         try{
-            Class.forName(DBDriver);
-            connection = DriverManager.getConnection(url,username,password);
+//            Class.forName(DBDriver);
+//            connection = DriverManager.getConnection(url,username,password);
+            connection = dataSource.getConnection();
         }catch (SQLException se){
             se.printStackTrace();
         }catch (Exception e){
